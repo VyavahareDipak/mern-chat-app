1a774ff44b3c55f2a7d088b4ae7504e79dbbbe00
DipakVyavahre <vyavahared7@gmail.com>
2024-04-23 19:51:37 +0530

    deployment changes added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+//imp for deploy:1
+const path =require("path") ;
 const express= require("express") 
 const dotenv = require("dotenv")
 const cookieparser = require("cookie-parser")
@@ -5,9 +7,12 @@ const authroutes = require("./routes/auth.routes")
 const messageroutes = require("./routes/message.routes")
 const userroutes = require("./routes/user.routes")
 const {app,server} = require("./socket/socket")
-dotenv.config() ;
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
+
+__dirname = path.resolve() ;
+
+dotenv.config() ;
 
 app.use(express.json()) ;
 app.use(cookieparser()) ;
@@ -15,6 +20,19 @@ app.use(cookieparser()) ;
 app.use("/api/auth",authroutes) ;
 app.use("/api/message",messageroutes) ;
 app.use("/api/user",userroutes) ;
+
+//deploy 3
+//use server static files like html,css ,image, sound etc.  dist folder will create when we build vite frontend .(build frontend npm run build)
+app.use(express.static(path.join(__dirname,"/frontend/dist"))) ;   
+
+//deploy 4 . go in package.json
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+//deploy 5 add following in package.json
+// "start":"node backend/server.js",
+//     "build":"npm install && npm install --prefix frontend && npm run build --prefix frontend"
 
 server.listen(PORT,(err)=>{
 if(err){
